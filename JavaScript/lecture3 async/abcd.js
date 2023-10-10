b68bb41c387201642494b546637f1b62880a0e83
laksh-krishna-sharma <asklakshsharma@gmail.com>
2023-10-10 23:36:58 +0530

    async

---
+++ JavaScript/lecture3 async/abcd.js
@@ -0,0 +1,6 @@
+// sync aur async kya ho ta 
+
+// sync matlab jab tak ek command pura na ho dusra nahi ho ga  
+
+// async matlab sare kaam shuru kr do jo kaam phela ho ja aya us ka jawab de de na 
+
