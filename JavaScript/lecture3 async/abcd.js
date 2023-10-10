641cac23ccaa7619d6b308fdcbc6615c5a243a28
laksh-krishna-sharma <asklakshsharma@gmail.com>
2023-10-10 23:53:34 +0530

    async

--- JavaScript/lecture3 async/abcd.js
+++ JavaScript/lecture3 async/abcd.js
@@ -4,3 +4,12 @@
 
 // async matlab sare kaam shuru kr do jo kaam phela ho ja aya us ka jawab de de na 
 
+// agar code mai kahi bhi setTimeout, setInterval, Promise, fetch, axios, ya XMLHttpRequest likkha hua milla to wo async js hai 
+
+// setTimeout(callbackFunction,timeInMs)
+// callback function hamesha async code mai answer aa ne ke baad chl ta hai 
+console.log("hi");
+setTimeout(function () {
+    console.log("hi after 2 second");
+},2000)
+
